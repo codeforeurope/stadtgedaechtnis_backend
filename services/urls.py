659dea5197eb23f6b5cf1b5ff53b55800923fd1e
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-05-22 13:59:36 +0200

    New APIs

--- services/urls.py
+++ services/urls.py
@@ -30,6 +30,16 @@ urlpatterns = patterns('',
     url(r'^locations/(?P<lat>\d{1,3}\.\d{1,10})/(?P<maxlat>\d{1,3}\.\d{1,10})/'
         '(?P<lon>\d{1,3}\.\d{1,10})/(?P<maxlon>\d{1,3}\.\d{1,10})/stories/$', GetLocationsWithStories.as_view(),
         name="get-locations-with-stories"),
+    url(r'^locations/(?P<id>\d+)/$', GetSingleLocation.as_view(),
+        name="get-location"),
+    url(r'^locations/(?P<id>\d+)/story_count/$', GetLocationWithStoryCount.as_view(),
+        name="get-location-with-story-count"),
+    url(r'^locations/(?P<id>\d+)/title/$', GetLocationWithStoryTitle.as_view(),
+        name="get-location-with-story-title"),
+    url(r'^locations/(?P<id>\d+)/stories/$', GetLocationWithStories.as_view(),
+        name="get-location-with-stories"),
+    url(r'^locations/stories/$', GetAllStories.as_view(), name="get-all-stories"),
+    url(r'^locations/stories/title$', GetAllStories.as_view(), name="get-all-stories"),
     url(r'^import-entry/(?P<id>\d+)/(?P<location>\d+)/$', ImportEntry.as_view(),
         name="import-entry")
     )
--- services/views.py
+++ services/views.py
@@ -4,6 +4,7 @@ from django.views.generic import View
 from django.http import HttpResponse
 from SPARQLWrapper import SPARQLWrapper, JSON
 from stadtgedaechtnis_backend.utils import get_nearby_locations
+from stadtgedaechtnis_backend.models import Entry, Location
 
 import jsonpickle
 import json
@@ -80,13 +81,16 @@ class GetLocationJSONView(View):
         pass
 
     def get(self, request, *args, **kwargs):
-        locations = get_nearby_locations(kwargs["lat"], kwargs["lon"],
-                                         kwargs["maxlat"] if "maxlat" in kwargs else 0,
-                                         kwargs["maxlon"] if "maxlon" in kwargs else 0)
+        locations = self.get_locations(kwargs)
 
         result = self.create_result_list(locations)
 
         return HttpResponse(jsonpickle.encode(result, unpicklable=False), content_type=RETURN_TYPE_JSON)
+
+    def get_locations(self, kwargs):
+        return get_nearby_locations(kwargs["lat"], kwargs["lon"],
+                                    kwargs["maxlat"] if "maxlat" in kwargs else 0,
+                                    kwargs["maxlon"] if "maxlon" in kwargs else 0)
 
 
 class GetLocationsWithStoryCount(GetLocationJSONView):
@@ -98,6 +102,24 @@ class GetLocationsWithStoryCount(GetLocationJSONView):
 
     def add_additional_location_info(self, result_location, location):
         result_location['story_count'] = str(location.entry_set.count())
+
+
+class GetSingleLocation(GetLocationJSONView):
+    """
+    Returns just a single location.
+    """
+
+    def get_locations(self, kwargs):
+        result = list()
+        result.append(Location.objects.get(pk=kwargs["id"]))
+        return result
+
+
+class GetLocationWithStoryCount(GetLocationsWithStoryCount, GetSingleLocation):
+    """
+    Same as above but for a single location
+    """
+    pass
 
 
 class GetLocationsWithStoryTitle(GetLocationJSONView):
@@ -118,9 +140,74 @@ class GetLocationsWithStoryTitle(GetLocationJSONView):
         pass
 
 
+class GetLocationWithStoryTitle(GetLocationsWithStoryTitle, GetSingleLocation):
+    """
+    Same as above but for a single location
+    """
+    pass
+
+
 class GetLocationsWithStories(GetLocationsWithStoryTitle):
     """
     Returns a list of locations and the stories attached to it.
+    """
+
+    def add_additional_story_info(self, result_story, story):
+        result_story["abstract"] = story.abstract
+        result_story["author"] = story.author
+        result_story["text"] = story.text
+        result_story["time_start"] = str(story.time_start)
+        if story.time_end is not None:
+            result_story["time_end"] = str(story.time_end)
+        result_story["type"] = str(story.type)
+        if story.mediaobject_set.count > 0:
+            media_object = story.mediaobject_set.first()
+            if media_object is not None:
+                result_story_image = dict()
+                result_story_image["src"] = media_object.mediasource_set.first().file.url
+                result_story_image["alt"] = media_object.alt
+                result_story["image"] = result_story_image
+
+
+class GetLocationWithStories(GetLocationsWithStories, GetSingleLocation):
+    """
+    Same as above but for a single location
+    """
+    pass
+
+
+class GetAllStoriesJSONView(View):
+    """
+    Returns a list of all stories with or without a location.
+    """
+
+    def get(self, request, *args, **kwargs):
+        stories = Entry.objects.all()
+
+        result = self.create_result_list(stories)
+
+        return HttpResponse(jsonpickle.encode(result, unpicklable=False), content_type=RETURN_TYPE_JSON)
+
+    def create_result_list(self, stories):
+        result = dict()
+        result["stories"] = list()
+        for story in stories:
+            result_story = dict()
+            result_story["title"] = story.title
+            if story.location is not None:
+                result_story["location"] = story.location.id
+            self.add_additional_story_info(result_story, story)
+            result["stories"].append(result_story)
+
+        return result
+
+    def add_additional_story_info(self, result_story, story):
+        pass
+
+
+class GetAllStories(GetAllStoriesJSONView):
+    """
+    Returns a list of all the saved stories and their complete information.
     """
 
     def add_additional_story_info(self, result_story, story):
