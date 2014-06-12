95bd03581b8b98e525c243183252953fd65c6200
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-06-12 08:53:19 +0200

    Minor service changes

--- urls.py
+++ urls.py
@@ -6,6 +6,7 @@ Created on 26.02.2014
 
 from django.conf.urls import patterns, url, include
 
-urlpatterns = patterns('',
-                       url(r'^services/', include('stadtgedaechtnis_backend.services.urls')),
-                       )
+urlpatterns = patterns(
+   '',
+   url(r'^services/', include('stadtgedaechtnis_backend.services.urls')),
+   )
