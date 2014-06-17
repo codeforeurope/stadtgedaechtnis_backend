cf251655a0b4d7901d6323c81220f8863fff8f60
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-06-17 18:36:57 +0200

    Add comments to serializers

--- serializers.py
+++ serializers.py
@@ -5,6 +5,9 @@ from stadtgedaechtnis_backend.models import *
 
 
 class LocationSerializer(serializers.ModelSerializer):
+    """
+    Serializes a location
+    """
     class Meta:
         model = Location
         fields = ('id', 'label', 'description', 'latitude', 'longitude', 'altitude',
@@ -12,6 +15,9 @@ class LocationSerializer(serializers.ModelSerializer):
 
 
 class LocationSerializerWithStoryIDs(serializers.ModelSerializer):
+    """
+    Serializes a location and the IDs of attached stories
+    """
     class Meta:
         model = Location
         fields = ('id', 'label', 'description', 'latitude', 'longitude', 'altitude',
@@ -19,12 +25,18 @@ class LocationSerializerWithStoryIDs(serializers.ModelSerializer):
 
 
 class StoryTitleSerializer(serializers.ModelSerializer):
+    """
+    Serializes a story with title and location
+    """
     class Meta:
         model = Story
         fields = ('id', 'title', 'location')
 
 
 class LocationSerializerWithStoryTitle(serializers.ModelSerializer):
+    """
+    Serializes a location with stories using the StoryTitleSerializer
+    """
     class Meta:
         model = Location
         fields = ('id', 'label', 'description', 'latitude', 'longitude', 'altitude',
@@ -34,7 +46,13 @@ class LocationSerializerWithStoryTitle(serializers.ModelSerializer):
 
 
 class AssetURLSerializer(serializers.ModelSerializer):
+    """
+    Serializes an asset, providing the first source URL
+    """
     class AssetSourceSerializer(serializers.URLField):
+        """
+        Serializes the first asset source to a source URL
+        """
         def to_native(self, value):
             if value.instance.sources.first() is not None:
                 return value.instance.sources.first().file.url
@@ -49,6 +67,9 @@ class AssetURLSerializer(serializers.ModelSerializer):
 
 
 class StoryImageSerializer(serializers.ModelSerializer):
+    """
+    Serializes a story with only title and abstract and all the assets belonging to this story using the AssetURLSerializer
+    """
     class Meta:
         model = Story
         fields = ('id', 'title', 'abstract', 'assets')
@@ -57,10 +78,16 @@ class StoryImageSerializer(serializers.ModelSerializer):
 
 
 class LocationSerializerWithStoryImages(LocationSerializerWithStoryTitle):
+    """
+    Serializes a Location and the attached stories using the StoryImageSerializer
+    """
     stories = StoryImageSerializer(many=True)
 
 
 class StoryWithAssetSerializer(serializers.ModelSerializer):
+    """
+    Serializes a story and all the IDs of attached stories
+    """
     class Meta:
         model = Story
         fields = ('id', 'title', 'abstract', 'text', 'author',
@@ -68,13 +95,28 @@ class StoryWithAssetSerializer(serializers.ModelSerializer):
 
 
 class StoryWithAssetImageSerializer(StoryWithAssetSerializer):
+    """
+    Serializes a story completely using AssetURLSerializer for its assets
+    """
     assets = AssetURLSerializer(many=False)
 
 
 class LocationSerializerWithStories(serializers.ModelSerializer):
+    """
+    Serializes a location with their attached stories using a StoryWithAssetSerializer
+    """
     class Meta:
         model = Location
         fields = ('id', 'label', 'description', 'latitude', 'longitude', 'altitude',
                   'created', 'modified', 'dbpedia_link', 'stories')
 
     stories = StoryWithAssetSerializer(many=True)
+
+
+class UserSerizalier(serializers.HyperlinkedModelSerializer):
+    """
+    Serializes a user with an absolute URL.
+    """
+    class Meta:
+        model = User
+        fields = ('url', 'id', 'username', 'first_name', 'last_name')
