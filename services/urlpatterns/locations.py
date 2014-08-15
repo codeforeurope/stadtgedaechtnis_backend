57e408a6ca5fe7915e06689d1983f9757f95bfdc
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-08-15 14:31:43 +0200

    fix typo

--- services/urlpatterns/locations.py
+++ services/urlpatterns/locations.py
@@ -32,7 +32,7 @@ urlpatterns = patterns(
     url(r'^(?P<lat>\d{1,3}\.\d{1,10})/(?P<maxlat>\d{1,3}\.\d{1,10})/'
         '(?P<lon>\d{1,3}\.\d{1,10})/(?P<maxlon>\d{1,3}\.\d{1,10})/stories/title/image/$',
         LocationListNearbyWithImages.as_view(),
-        name="get-locations-with-stories-image"),
+        name="get-near-locations-with-stories-image"),
     url(r'^(?P<pk>\d+)/$', SingleLocation.as_view(),
         name="get-location"),
     url(r'^(?P<pk>\d+)/stories/story_count/$', SingleLocationWithStoryIDs.as_view(),
